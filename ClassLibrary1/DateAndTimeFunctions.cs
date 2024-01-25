9c8c01e0263eba3977248337e3b2fe4490470052
iFyyyyyy <134650070+iFyyyyyy@users.noreply.github.com>
2024-01-25 16:29:35 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/DateAndTimeFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DateFunctionsLibrary;
+
+public class DateAndTimeFunctions
+{
+
+    /*
+     * Напишите функции для нахождения разницы между двумя датами, (+)
+     * определения, является ли год високосным, 
+     * нахождения времени суток по заданному времени, 
+     * а также для форматирования даты и времени. 
+     * Напишите модульные тесты для каждой функции.
+     */
+
+
+    // Сравнение двух дат.
+    public static DateTime DateCompare(DateTime Date1, DateTime Date2){
+        if (Date1 == Date2) {
+            throw new Exception("Dates are equal!");
+        }
+        else {
+            int result = DateTime.Compare(Date1, Date2);
+            if (result < 0){
+                return Date1;
+            }
+            else 
+                return Date2;
+        }
+    }
+
+    //end
+
+
+
+
+}
---
+++ WinFormsApp1/Program.cs
@@ -0,0 +1,31 @@
+using DateFunctionsLibrary;
+
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+
+            // test
+            DateTime date1 = new DateTime(2009, 8, 1, 0, 0, 0);
+            DateTime date2 = new DateTime(2009, 8, 1, 12, 0, 0);
+            DateAndTimeFunctions.DateCompare(date1, date2);
+            
+
+
+
+
+
+        }
+    }
+}
